d6c585268f03ad1bd5ac38dc2a86715287d65c6e
korey <75332399+kor3y@users.noreply.github.com>
2021-01-03 19:35:23 +0200

    Update and rename __resource.lua to fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'korey#5241'
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+client_scripts{'lib/*', 'client.lua'}
+server_scripts{'@vrp/lib/utils.lua', 'server.lua'}
+ui_page 'html/ui.html'
+files{'img/dosardmv.png', 'img/permarma.png', 'img/asiguauto.png', 'html/ui.html', 'html/dmv.png', 'html/cursor.png', 'html/styles.css', 'html/questions.js', 'html/scripts.js', 'html/debounce.min.js', 'stream/averta.gfx'}
